cc9f57e046f30cce6cf233fddc0ce078847a737a
Harshal <harshalk9764@gmail.com>
2025-01-02 10:55:31 +0530

    etl code added

--- etl.py
+++ etl.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 
+st.write('AVD')
 data = {
         'Task': ['Extract', 'Transform', 'Load'],
         'Status': ['Completed', 'InProgress', 'Pending'] 
